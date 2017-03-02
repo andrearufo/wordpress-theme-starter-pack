e0954cf8e1405190907ecb8cfa96ac148778103e
andrearufo <orangedropdesign@gmail.com>
2017-03-02 17:06:33 +0100

    - Removed some old functions

--- functions.php
+++ functions.php
@@ -56,14 +56,6 @@ function odd_my_scripts_method() {
 		'0.1',
 		true
 	);
-
-	wp_enqueue_script(
-		'acfpromaps',
-		get_template_directory_uri() . '/dist/scripts/acfpromaps.js',
-		array('googleapis'),
-		'0.1',
-		true
-	);
 	
 }
 
@@ -97,82 +89,8 @@ function odd_content_width() {
 }
 add_action( 'after_setup_theme', 'odd_content_width', 0 );
 
-/* Custom pagination */
+/* Get the post thumbnail url */
 
-function custom_pagination($numpages = '', $pagerange = '', $paged='') {
-	
-	if (empty($pagerange)) {
-		$pagerange = 2;
-	}
-
-	/**
-	 * This first part of our function is a fallback
-	 * for custom pagination inside a regular loop that
-	 * uses the global $paged and global $wp_query variables.
-	 * 
-	 * It's good because we can now override default pagination
-	 * in our theme, and use this function in default quries
-	 * and custom queries.
-	 */
-	 
-	global $paged;
-	
-	if (empty($paged)) {
-		$paged = 1;
-	}
-	
-	if ($numpages == '') {
-		global $wp_query;
-	
-		$numpages = $wp_query->max_num_pages;
-	
-		if(!$numpages) {
-			$numpages = 1;
-		}
-	}
-	
-	/** 
-	 * We construct the pagination arguments to enter into our paginate_links
-	 * function. 
-	 */
-	 
-	$pagination_args = array(
-		'base'			=> get_pagenum_link(1) . '%_%',
-		'format'		=> 'page/%#%',
-		'total'			=> $numpages,
-		'current'		=> $paged,
-		'show_all'		=> False,
-		'end_size'		=> 1,
-		'mid_size'		=> $pagerange,
-		'prev_next'		=> True,
-		'prev_text'		=> '<i class="fa fa-arrow-circle-left"></i>',
-		'next_text'		=> '<i class="fa fa-arrow-circle-right"></i>',
-		'type'			=> 'plain',
-		'add_args'		=> false,
-		'add_fragment'	=> ''
-	);
-	
-	$paginate_links = paginate_links($pagination_args);
-	
-	if ($paginate_links) {
-	
-		echo '<nav class="custom-pagination">';
-	
-	//	echo '<span class="page-numbers page-num">Pagina ' . $paged . ' di ' . $numpages . '</span> ';
-		echo $paginate_links;
-	
-		echo '</nav>';
-	
-	}
-	
-}
-
-if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_page();
-	
-}
-	
 function odd_post_thumbnail_url($size = 'large', $id = null){
 
 	if( is_null($id) ) $id = $post->id;
@@ -182,64 +100,4 @@ function odd_post_thumbnail_url($size = 'large', $id = null){
 
 	return $image[0];
 
-}
-
-function odd_post_thumbnail($size = 'large', $attr = null){
-
-	if( is_null($attr) ){
-		$attr = array(
-			'src'	=> $src,
-			'class'	=> "attachment-$size",
-			'alt'	=> trim(strip_tags( $wp_postmeta->_wp_attachment_image_alt )),
-		);
-	}
-
-	if ( has_post_thumbnail() ) {
-	
-		the_post_thumbnail($size, $attr);
-	
-	}else{
-
-		$default = get_field('default_post_thumbnail', 'option');
-		echo '<img class="'.$attr['class'].'" alt="'.$default['alt'].'" src="'.$default['sizes'][$size].'">';
-	
-	} 
-
-}
-
-/* Posts correlati */
-
-function related_posts() {
-	global $post;
-	$tags = wp_get_post_tags( $post->ID );
-
-	if($tags) {
-	
-		foreach( $tags as $tag ) {
-			$tag_arr .= $tag->slug . ',';
-		}
-	
-		$args = array(
-			'tag' => $tag_arr,
-			'numberposts' => 3, // Set the amount of posts you wish to display
-			'post__not_in' => array($post->ID)
-		);
-	
-		$related_posts = get_posts( $args );
-	
-		if($related_posts) {
-			
-			echo '<h4 class="text-uppercase">'._('Forse potrebbe interessarti anche...').'</h4>';
-		
-			foreach ( $related_posts as $post ) : 
-
-				setup_postdata( $post ); 
-				get_template_part('part-articlebox');
-
-			endforeach;
-
-			wp_reset_postdata();
-
-		}
-	}
 }
