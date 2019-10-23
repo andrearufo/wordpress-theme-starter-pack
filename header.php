704b00b5a1dfa0e48ea5c770b0e9133e3b1a99f0
andrearufo <info@andrearufo.it>
2019-10-23 17:04:27 +0200

    Add shortcut rel

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
 	<?php wp_head() ?>
 
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/images/favicons/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/images/favicons/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/images/favicons/favicon-16x16.png">
+	<link rel="shortcut icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/images/favicons/favicon-32x32.png">
+	<link rel="shortcut icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/images/favicons/favicon-16x16.png">
 	<link rel="manifest" href="<?php echo get_template_directory_uri() ?>/images/favicons/site.webmanifest">
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri() ?>/images/favicons/mstile-144x144.png">
